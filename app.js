442bf5bc7bf34b1f2fd05b362e1fea4a077ff797
Rafael Herbster <rafaelherbster8@gmail.com>
2025-01-18 09:24:37 -0300

    implementar Array para armazenar nomes e função que agrega nomes e apaga o campo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.querySelector('input').value;
+    if (amigo == "") {
+        alert("Porfavor insira um nome.")
+    } else {
+        listaAmigos.push(amigo);
+        console.log(listaAmigos);
+        limparCampo();
+    }
+  
+}
+
+function limparCampo() {
+    amigo = document.querySelector('input');
+    amigo.value = "";
+    
+}
