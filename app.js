9a5fbeec04ce39f6e4e6ecb1420939920fb62f97
Rafael Herbster <rafaelherbster8@gmail.com>
2025-01-18 10:49:57 -0300

    Implentar a função que sorteia e mostra na tela o amigo sorteado + comentarios

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ let listaAmigos = [];
 
 function adicionarAmigo() {
     let amigo = document.querySelector('input').value;
-    if (amigo == "") {
-        alert("Porfavor insira um nome.")
+    if (amigo == "") {//verifica se o campo estava vazio.
+        alert("Porfavor insira um nome.");
     } else {
-        listaAmigos.push(amigo);
+        listaAmigos.push(amigo);//adiciona o novo amigo na lista.
         console.log(listaAmigos);
         limparCampo();
         mostrarAmigos();
@@ -15,18 +15,34 @@ function adicionarAmigo() {
 }
 
 function limparCampo() {
-    amigo = document.querySelector('input');
+    amigo = document.querySelector('input');//seleciona o campo de texto e limpa.
     amigo.value = "";
     
 }
 
 function mostrarAmigos() {
-    let userLista = document.getElementById("listaAmigos");
-    userLista.innerHTML = "";
-    listaAmigos.forEach(amigo =>{
-        let elemento = document.createElement("li");
-        elemento.textContent = amigo;
-        userLista.appendChild(elemento);
+    let userLista = document.getElementById("listaAmigos");//seleciona o elemento do HTML pelo ID.
+    userLista.innerHTML = "";// esvazia o valor anterior para que não ocorra duplicação de valores.
+    listaAmigos.forEach(amigo =>{//para cada "amigo" na lista "listaAmigos" faça o seguinte:
+        let elemento = document.createElement("li");//crie um elemento do tipo "<li>"
+        elemento.textContent = amigo;//atribua um texto.
+        userLista.appendChild(elemento);// e adicione o elemento dentro da tag <ul>
     });
 }
+
+function sortearAmigo() {
+    let indexSorteado = Math.floor(Math.random() * listaAmigos.length);//selecione um index aleatorio dentro lista "listaAmigos".
+    let amigoSorteado = listaAmigos[indexSorteado];//selecione o amigo a partir do index selecionado.
+    console.log(amigoSorteado, listaAmigos.length, indexSorteado);
+    mostrarAmigoSorteado(amigoSorteado);
+}
+
+function mostrarAmigoSorteado(userSorteado) {
+    let userResultado = document.getElementById("resultado");//seleciona o elemento do HTML pelo ID.
+    userResultado.innerHTML = "";// esvazia o valor anterior para que não ocorra duplicação de valores.
+    let elemento = document.createElement("li");//crie um elemento do tipo "<li>"
+    elemento.textContent = userSorteado;//atribua um texto.
+    userResultado.appendChild(elemento);// e adicione o elemento dentro da tag <ul>
+}
+
 
